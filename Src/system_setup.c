d3660ac23becceeded5d01c8a91bfd5f6d17657a
MrFangao <143060549+MrFangao@users.noreply.github.com>
2025-02-04 17:40:45 -0700

    blue and red

--- Src/system_setup.c
+++ Src/system_setup.c
@@ -1,6 +1,9 @@
 #include "main.h"
 #include <stm32f0xx_hal.h>
 
+void My_HAL_RCC_GPIOC_CLK_ENABLE(void) {
+    RCC->AHBENR |= RCC_AHBENR_GPIOCEN; // 使能 GPIOC 时钟
+}
 /**
   * @brief  This function is executed in case of error occurrence.
   * @param  None
